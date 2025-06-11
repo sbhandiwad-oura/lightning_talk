57930d99a9b028b4731b3fcff2db77ffc2848a7e
Sarah Bhandiwad <sarah.bhandiwad@ouraring.com>
2025-06-11 12:22:42 -0700

    Initial commit with colors

---
+++ Lightning.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+class Lightning {
+    let color1 = Color.red
+    let color2 = Color.orange
+    let color3 = Color.yellow
+    let color4 = Color.blue
+    let color5 = Color.purple
+    let color6 = Color.pink
+    let color7 = Color.green
+
+}
