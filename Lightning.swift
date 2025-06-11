c990488a366e17236d9406a256e640ea254d5765
Sarah Bhandiwad <sarah.bhandiwad@ouraring.com>
2025-06-11 12:29:52 -0700

    do something with the colors

--- Lightning.swift
+++ Lightning.swift
@@ -12,4 +12,16 @@ class Lightning {
     let color8 = Color.gray
     let color9 = Color.black
     let color10 = Color.white
+
+    func makeRainbow() -> [Color] {
+        return [color1, color2, color3, color4, color5, color6, color7]
+    }
+
+    func makeGrayscale() -> [Color] {
+        return [color8, color9, color10]
+    }
+
+    func makeAllColors() -> [Color] {
+        return [color1, color2, color3, color4, color5, color6, color7, color8, color9, color10]
+    }
 }
