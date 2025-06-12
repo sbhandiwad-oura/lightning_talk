2ff8cf4a04be7056266a1337ab1e55776c720b21
Sarah Bhandiwad <sarah.bhandiwad@ouraring.com>
2025-06-12 12:27:44 -0700

    add makePrimaryColors

--- Lightning.swift
+++ Lightning.swift
@@ -8,19 +8,23 @@ class Lightning {
     let color5 = Color.blue
     let color6 = Color.purple
     let color7 = Color.pink
-
+    
     let color8 = Color.gray
     let color9 = Color.black
     let color10 = Color.white
-
+    
+    func makePrimaryColors() -> [Color] {
+        return [color1, color3, color5]
+    }
+    
     func makeRainbow() -> [Color] {
         return [color1, color2, color3, color4, color5, color6, color7]
     }
-
+    
     func makeGrayscale() -> [Color] {
         return [color8, color9, color10]
     }
-
+    
     func makeAllColors() -> [Color] {
         return [color1, color2, color3, color4, color5, color6, color7, color8, color9, color10]
     }
