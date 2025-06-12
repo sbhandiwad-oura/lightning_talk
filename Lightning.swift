ef25acde61b5b02c8322d889094bfddf34a4a4da
Sarah Bhandiwad <sarah.bhandiwad@ouraring.com>
2025-06-12 12:26:29 -0700

    fix the order of the rainbow

--- Lightning.swift
+++ Lightning.swift
@@ -4,10 +4,10 @@ class Lightning {
     let color1 = Color.red
     let color2 = Color.orange
     let color3 = Color.yellow
-    let color4 = Color.blue
-    let color5 = Color.purple
-    let color6 = Color.pink
-    let color7 = Color.green
+    let color4 = Color.green
+    let color5 = Color.blue
+    let color6 = Color.purple
+    let color7 = Color.pink
 
     let color8 = Color.gray
     let color9 = Color.black
